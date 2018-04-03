204fd5a283583dcdd10093fe344f143a7aebadf6
HyunhooKwon <kwongusgn@gmail.com>
2018-04-03 13:07:28 +0900

    BigInter 예제 코드 작성.

--- Examples/src/chapter02/item01/BigInteger.java
+++ Examples/src/chapter02/item01/BigInteger.java
@@ -1,25 +1,63 @@
 package chapter02.item01;
 
-import java.util.Random;
-
 public class BigInteger {
-    private static final int SMALL_PRIME_THRESHOLD = 95;
-    private static final int DEFAULT_PRIME_CERTAINTY = 100;
+    final int signum;
+    final int[] mag;
 
-    public static BigInteger probablePrime(int bitLength, Random rnd) {
-        if (bitLength < 2)
-            throw new ArithmeticException("bitLength < 2");
+    private static final int MAX_MAG_LENGTH = Integer.MAX_VALUE / Integer.SIZE + 1; // (1 << 26)
 
-        return (bitLength < SMALL_PRIME_THRESHOLD ?
-                smallPrime(bitLength, DEFAULT_PRIME_CERTAINTY, rnd) :
-                largePrime(bitLength, DEFAULT_PRIME_CERTAINTY, rnd));
+    public static final BigInteger ZERO = new BigInteger(new int[0], 0);
+
+    public BigInteger(long val) {
+        if (val < 0) {
+            val = -val;
+            signum = -1;
+        } else {
+            signum = 1;
+        }
+
+        int highWord = (int)(val >>> 32);
+        if (highWord == 0) {
+            mag = new int[1];
+            mag[0] = (int)val;
+        } else {
+            mag = new int[2];
+            mag[0] = highWord;
+            mag[1] = (int)val;
+        }
     }
 
-    private static BigInteger smallPrime(int bitLength, int certainty, Random rnd) {
-        return null;
+    private final static int MAX_CONSTANT = 16;
+    private static BigInteger posConst[] = new BigInteger[MAX_CONSTANT+1];
+    private static BigInteger negConst[] = new BigInteger[MAX_CONSTANT+1];
+
+    public static BigInteger valueOf(long val) {
+        // If -MAX_CONSTANT < val < MAX_CONSTANT, return stashed constant
+        if (val == 0)
+            return ZERO;
+        if (val > 0 && val <= MAX_CONSTANT)
+            return posConst[(int) val];
+        else if (val < 0 && val >= -MAX_CONSTANT)
+            return negConst[(int) -val];
+
+        return new BigInteger(val);
     }
 
-    private static BigInteger largePrime(int bitLength, int certainty, Random rnd) {
-        return null;
+    BigInteger(int[] magnitude, int signum) {
+        this.signum = (magnitude.length == 0 ? 0 : signum);
+        this.mag = magnitude;
+        if (mag.length >= MAX_MAG_LENGTH) {
+            checkRange();
+        }
+    }
+
+    private void checkRange() {
+        if (mag.length > MAX_MAG_LENGTH || mag.length == MAX_MAG_LENGTH && mag[0] < 0) {
+            reportOverflow();
+        }
+    }
+
+    private static void reportOverflow() {
+        throw new ArithmeticException("BigInteger would overflow supported range");
     }
 }
