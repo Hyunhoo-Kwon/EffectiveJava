a7f3fa2c11b92ecf40f3b2febf98b8c1e3f6e1af
HyunhooKwon <kwongusgn@gmail.com>
2018-08-13 23:16:11 +0900

    BigIntegerTest 리팩토링.

--- Examples/src/test/java/chapter02/item01/BigIntegerTest.java
+++ Examples/src/test/java/chapter02/item01/BigIntegerTest.java
@@ -1,23 +1,51 @@
 package chapter02.item01;
 
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.assertSame;
+import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.core.IsSame.sameInstance;
+import static org.junit.Assert.assertThat;
 
 public class BigIntegerTest {
-    @Test
-    public void compareInstances() {
-        BigInteger instanceConstructor1 = new BigInteger(0);
-        BigInteger instanceConstructor2 = new BigInteger(0);
-        BigInteger instanceFactoryMethod1 = BigInteger.valueOf(0);
-        BigInteger instanceFactoryMethod2 = BigInteger.valueOf(0);
-        BigInteger instanceConstant = BigInteger.ZERO;
+    BigInteger instanceConstructor = new BigInteger(0);
+    BigInteger instanceFactoryMethod = BigInteger.valueOf(0);
+    BigInteger instanceConstant = BigInteger.ZERO;
 
-        assertNotSame(instanceConstructor1, instanceConstructor2);
-        assertNotSame(instanceConstructor1, instanceFactoryMethod1);
-        assertSame(instanceFactoryMethod1, instanceFactoryMethod2);
-        assertNotSame(instanceConstructor1, instanceConstant);
-        assertSame(instanceFactoryMethod1, instanceConstant);
+    @Test
+    public void compareInstanceConstructorWithOthers() {
+        assertThat(instanceConstructor, notSameInstance(getNewInstanceConstructor()));
+        assertThat(instanceConstructor, notSameInstance(instanceFactoryMethod));
+        assertThat(instanceConstructor, notSameInstance(instanceConstant));
+    }
+
+    @Test
+    public void compareInstanceFactoryMethodWithOthers() {
+        assertThat(instanceFactoryMethod, notSameInstance(instanceConstructor));
+        assertThat(instanceFactoryMethod, sameInstance(getNewInstanceFactoryMethod()));
+        assertThat(instanceFactoryMethod, sameInstance(instanceConstant));
+    }
+
+    @Test
+    public void compareInstanceConstantMethodWithOthers() {
+        assertThat(instanceConstant, notSameInstance(instanceConstructor));
+        assertThat(instanceConstant, sameInstance(instanceFactoryMethod));
+        assertThat(instanceConstant, sameInstance(getNewInstanceConstant()));
+    }
+
+    private Matcher<BigInteger> notSameInstance(BigInteger instance) {
+        return not(sameInstance(instance));
+    }
+
+    private BigInteger getNewInstanceConstructor() {
+        return new BigInteger(0);
+    }
+
+    private BigInteger getNewInstanceFactoryMethod() {
+        return BigInteger.valueOf(0);
+    }
+
+    private BigInteger getNewInstanceConstant() {
+        return BigInteger.ZERO;
     }
 }
